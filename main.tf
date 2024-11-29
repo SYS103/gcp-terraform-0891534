85842527fd6f5cf28bc386af9859a60c6af2699f
StefanYS <stefan.sutanto@student.odisee.be>
2024-11-29 23:40:09 +0100

    project commit

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "cicd-bucket" {
   name                     = "tt-cicd-bucket"
+  project                  = "CICD-Dev-r0891534"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
